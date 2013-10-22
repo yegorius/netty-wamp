4135acefc14791cab7b064285f7f9f7a7d327a46
yegorius <yegorius@domic.us>
2013-10-22 21:10:32 +0300

    minor changes

--- src/main/java/io/netty/protocol/wamp/server/CallErrorException.java
+++ src/main/java/io/netty/protocol/wamp/server/CallErrorException.java
@@ -1,4 +1,21 @@
 package io.netty.protocol.wamp.server;
 
-public class CallErrorException extends Exception {
+import java.io.IOException;
+
+public class CallErrorException extends IOException {
+	public CallErrorException() {
+		super();
+	}
+
+	public CallErrorException(String message) {
+		super(message);
+	}
+
+	public CallErrorException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public CallErrorException(Throwable cause) {
+		super(cause);
+	}
 }
